500913b4ea05d987278bcc8af01e050dc5002374
Dmitry Osipov <dimaosipov00@gmail.com>
2023-11-13 19:11:43 +0300

    Added a new field that calculates the number of characters for the "content" block of a specific record from the database. Added 2 methods for changing the "is_published" flag

--- women/admin.py
+++ women/admin.py
@@ -1,4 +1,6 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.http import HttpRequest
+
 from .models import Women, Category
 
 # Register your models here.
@@ -13,13 +15,48 @@ class WomenAdmin(admin.ModelAdmin):
     ordering - tuple - порядок сортировки (если указано 2 и более поля, то сначала будет сортироваться по первому полю,
     а затем, если будут совпадения, то по второму полю);\n
     list_editable - tuple - список полей, которые можно редактировать;\n
-    list_per_page - int - максимальное количество отображаемых статей (пагинация реализуется автоматически).
+    list_per_page - int - максимальное количество отображаемых статей (пагинация реализуется автоматически);\n
+    actions - tuple - список действий (посредством функций).
     """
-    list_display = ('id', 'title', 'time_create', 'is_published', 'cat')
-    list_display_links = ('id', 'title')
+    list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
+    list_display_links = ('title', )
     ordering = ('time_create', 'title')
     list_editable = ('is_published', )  # ВАЖНО: редактируемое поле не может быть кликабельным.
     list_per_page = 5
+    actions = ('set_published', 'set_draft')
+
+    @admin.display(description='Краткое описание', ordering='content')
+    def brief_info(self, women: Women) -> str:
+        """
+        Метод отвечает за создание дополнительного поля с указанием длины блока content конкретной записи. Дополнительное
+        поле в БД отсутствует и не сохраняется туда.
+
+        :param women: Таблица women_women базы данных.
+        :return: Дополнительное поле с информацией о количестве символов блока content конкретной записи.
+        """
+        return f'Описание {len(women.content)} символов.'
+
+    @admin.action(description='Опубликовать выбранные записи')
+    def set_published(self, request: HttpRequest, queryset) -> None:
+        """
+        Метод будет устанавливать статус публикации в состояние "опубликовано" для выбранных постов.
+
+        :param request: Запрос пользователя.
+        :param queryset: Менеджер модели, устанавливает is_published на "опубликовано".
+        """
+        count = queryset.update(is_published=Women.Status.PUBLISHED)
+        self.message_user(request, f'Изменено {count} записей')
+
+    @admin.action(description='Снять с публикации выбранные записи')
+    def set_draft(self, request: HttpRequest, queryset) -> None:
+        """
+        Метод будет устанавливать статус публикации в состояние "черновик" для выбранных постов.
+
+        :param request: Запрос пользователя.
+        :param queryset: Менеджер модели, устанавливает is_published на "черновик".
+        """
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        self.message_user(request, f'{count} записей снято с публикации!', messages.WARNING)
 
 
 @admin.register(Category)
