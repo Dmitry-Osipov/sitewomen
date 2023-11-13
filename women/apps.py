458c647e392ddb838eda0abeeeb70c77ae3d57dd
Dmitry Osipov <dimaosipov00@gmail.com>
2023-11-13 16:01:54 +0300

    Added a site to the admin panel. Created a superuser. Changed the project language to Russian. Updated UI/UX for admin panel

--- women/apps.py
+++ women/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class WomenConfig(AppConfig):
+    verbose_name = 'Женщины мира'  # Меняем название заголовка таблицы модели в админ-панели.
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'women'
