5478f57381757a38abe8ac9df2cb05a442d547bb
Dmitry Osipov <dimaosipov00@gmail.com>
2023-11-14 13:11:12 +0300

    Added a helper class for filtering in the admin panel. Added the ability to search for an article by title. Added filters by criteria: married, category, publication status

--- women/admin.py
+++ women/admin.py
@@ -4,6 +4,46 @@ from django.http import HttpRequest
 from .models import Women, Category
 
 # Register your models here.
+class MarriedFilter(admin.SimpleListFilter):
+    """
+    Вспомогательный класс для WomenAdmin. Нужен для создания фильтра по связанной таблице husband класса Women. Фильтр
+    будет отбирать замужних и незамужних женщин.
+
+    Атрибуты:\n
+    title - str - название фильтра;\n
+    parameter_name - str - ключ для GET-запроса.
+    """
+    title = 'Статус женщин'
+    parameter_name = 'status'
+
+    def lookups(self, request: HttpRequest, model_admin: admin.ModelAdmin) -> list[tuple[str, str]]:
+        """
+        Метод нужен для настройки нового люкапа фильтрации.
+
+        :param request: Запрос пользователя.
+        :param model_admin: Класс отображения статей.
+        :return: Значение параметра parameter_name + кликабельное название позиции для пользователя.
+        """
+        return [
+            ('married', 'Замужем'),
+            ('single', 'Не замужем'),
+        ]
+
+    def queryset(self, request: HttpRequest, queryset):
+        """
+        Метод возвращает отфильтрованные посты. По умолчанию возвращаются все посты.
+
+        :param request: Запрос пользователя.
+        :param queryset: Менеджер модели.
+        :return: Отфильтрованный QuerySet по нужным значениям
+        """
+        if self.value() == 'married':
+            return queryset.filter(husband__isnull=False)
+
+        if self.value() == 'single':
+            return queryset.filter(husband__isnull=True)
+
+
 @admin.register(Women)  # Регистрируем модель.
 class WomenAdmin(admin.ModelAdmin):
     """
@@ -16,7 +56,9 @@ class WomenAdmin(admin.ModelAdmin):
     а затем, если будут совпадения, то по второму полю);\n
     list_editable - tuple - список полей, которые можно редактировать;\n
     list_per_page - int - максимальное количество отображаемых статей (пагинация реализуется автоматически);\n
-    actions - tuple - список действий (посредством функций).
+    actions - tuple - список действий (посредством функций);\n
+    search_fields - tuple - список полей, по которым будет осуществляться поиск статьи по символам;\n
+    list_filter - tuple - список отвечает за специальный блок, в котором можно указать фильтрацию по указанным полям.
     """
     list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
     list_display_links = ('title', )
@@ -24,6 +66,10 @@ class WomenAdmin(admin.ModelAdmin):
     list_editable = ('is_published', )  # ВАЖНО: редактируемое поле не может быть кликабельным.
     list_per_page = 5
     actions = ('set_published', 'set_draft')
+    search_fields = ('title', )  # ВАЖНО: искать возможно только по полям (т.е. по 'cat' поиск не осуществить напрямую,
+    # ибо это внешний ключ). Если требуется всё же искать через категорию, то требуется указать поля связанной таблицы
+    # либо люкапы: "cat__name" - поиск через категорию, "title__startswith" - поиск фрагмента только с начала строки.
+    list_filter = (MarriedFilter, 'cat__name', 'is_published')
 
     @admin.display(description='Краткое описание', ordering='content')
     def brief_info(self, women: Women) -> str:
